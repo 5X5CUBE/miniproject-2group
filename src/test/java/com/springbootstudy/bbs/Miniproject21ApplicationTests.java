867ced3cab6ef770e694d0e300b10d11bbb82b53
5x5cube <db5526@naver.com>
2026-01-13 15:02:35 +0900

    기본파일 커밋

---
+++ src/test/java/com/springbootstudy/bbs/Miniproject21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootstudy.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniproject21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
